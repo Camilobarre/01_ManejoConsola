dd815c654ecd5b0d2293c57efccb56dc52580ac2
Camilobarre <barrecami@gmail.com>
2024-04-15 11:41:26 -0500

    Se terminan los metodos para imprimir en consola

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,38 @@
-var UserName="Camilo"
-console.log(UserName)
+let userName="Camilo"
+let lastName="Barreneche Escobar";
+const age="27 años";
+const email="barrecami@gmail.com"
+let phoneNumber="+573015468063"
+let address="Carrera 81 #32C 14"
+
+// Console Methods
+console.log(userName)
+console.info(lastName)
+console.warn(age)
+console.error(email)
+console.debug(phoneNumber)
+alert(address)
+console.log(`%c${address}`, "background-color:blue; color:white; border-radius: 50px; font-size: 50px")
+
+let timerInterval;
+Swal.fire({
+  title: "Auto close alert!",
+  html: "I will close in <b></b> milliseconds.",
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading();
+    const timer = Swal.getPopup().querySelector("b");
+    timerInterval = setInterval(() => {
+      timer.textContent = `${Swal.getTimerLeft()}`;
+    }, 100);
+  },
+  willClose: () => {
+    clearInterval(timerInterval);
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log("I was closed by the timer");
+  }
+});
