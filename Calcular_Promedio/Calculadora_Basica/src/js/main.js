a9493b27f4cf9438f7130930feef81867ebc4b69
Camilobarre <barrecami@gmail.com>
2024-04-16 07:21:09 -0500

    Se realiza el ejercico de calcular el promedio de unas notas

--- Calcular_Promedio/Calculadora_Basica/src/js/main.js
+++ Calcular_Promedio/Calculadora_Basica/src/js/main.js
@@ -1,12 +1,22 @@
-numero1= Number(prompt("Ingresa el primer número:"))
-numero2= Number(prompt("Ingresa el segundo número:"))
+let notas=[]
 
-suma=numero1+numero2
-resta=numero1-numero2
-multiplicacion=numero1*numero2
-division=numero1/numero2
+let nota1=parsefloat(prompt("Ingresa primera nota:"))
+let nota2=parsefloat(prompt("Ingresa segunda nota:"))
+let nota3=parsefloat(prompt("Ingresa tercera nota:"))
+let nota4=parsefloat(prompt("Ingresa cuarta nota:"))
+let nota5=parsefloat(prompt("Ingresa quinta nota:"))
 
-console.info("La suma es:"+suma)
-console.info("La resta es:"+resta)
-console.info("La multiplicación es:"+multiplicacion)
-console.info("La división es:"+division)
+notas.push(nota1,nota2,nota3,nota4,nota5)
+
+let totalNotas= 0;
+for (let i=0; i<notas.length; i++){
+    totalNotas+=notas[i];
+}
+
+let promedio= totalNotas / notas.length;
+
+if (promediopromedio<3.0){
+    console.log("Tu promedio es de: "+promedio+". "+"Aprobaste");
+} else { 
+    console.log("Tu promedio es de: "+ promedio +". "+ "No aprobaste");
+}
